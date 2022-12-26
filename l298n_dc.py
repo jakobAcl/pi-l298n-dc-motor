e61abc71f2466a5137db02cc8f68fd830654ebfb
Jakob Acl <jakob.achleitner.2007@gmail.com>
2022-12-26 10:43:31 +0100

    exit-Hilfe

--- l298n_dc.py
+++ l298n_dc.py
@@ -23,6 +23,8 @@ print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
+print("push 'e' to exit")
+print("\n")
 
 while(1):
 
